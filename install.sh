9afbb2fefda950c089427fdef26995281eeac0bb
SemiDoge <taelforlife@gmail.com>
2023-05-27 20:48:37 -0400

    Improve install script

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 set -xe
 sed -i 's/#define DEBUG/#define RELEASE/' inc/constants.h
+sed -i 's/set(DEBUG True)/set(DEBUG False)/' CMakeLists.txt
 ./build.sh
 sed -i 's/#define RELEASE/#define DEBUG/' inc/constants.h
+sed -i 's/set(DEBUG False)/set(DEBUG True)/' CMakeLists.txt
 
 sudo cp build/romManager /usr/bin/
 mkdir -p ~/.config/romManager
